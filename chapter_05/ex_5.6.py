aa9e28ca7d4ea8d77ba7b661f2fca405de1c3be7
Álisson Alves <alissoncc.oliveira@gmail.com>
2023-11-09 20:29:51 -0300

    Continuation of the exercises in chapter 5

--- chapter_05/ex_5.6.py
+++
@@ -1,8 +0,0 @@
-import time
-counter = 1
-table_number = int(input("Enter a value to calculate the table: "))
-
-while counter <= 10:
-    print(table_number, "x", counter, "=", table_number * counter)
-    counter += 1
-    time.sleep(1)
